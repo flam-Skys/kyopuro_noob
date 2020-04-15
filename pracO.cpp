c8b34980d935da10a5c9316f3832f3ca0fd36608
flam-Skys <54589059+flam-Skys@users.noreply.github.com>
2020-04-15 16:29:05 +0900

    Add files via upload

---
+++ pracO.cpp
@@ -0,0 +1,10 @@
+    #include <bits/stdc++.h>
+    using namespace std;
+     
+    int main() {
+      vector<int> height(3);
+      cin >> height.at(0) >> height.at(1) >> height.at(2);
+
+      sort(height.begin(), height.end());
+      cout << height.at(2) - height.at(0) << endl;
+    }
